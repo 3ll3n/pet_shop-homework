374e96651eafff6a29e1bdbe6d0eb01b1ed889ba
3ll3n <ellengrafton01@gmail.com>
2017-01-13 13:47:00 +0000

    added first six tests

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,3 +1,27 @@
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, cash)
+ return pet_shop[:admin][:total_cash] += cash
+end
+
+def pets_sold(sold)
+  return sold[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, sold)
+  return pet_shop[:admin][:pets_sold] += sold
+end
+
+def stock_count(stock)
+ return stock[:pets].count
+end
+
+def pets_by_breed(pet_shop, breed)
+  return breed[:pets][:breed]["British Shorthair"]
+end
